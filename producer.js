400ef986b34be9e155e26b555adf5adea38987bf
Jason Post <jason.w.post@gmail.com>
2017-12-08 16:04:23 +0100

    refactor

--- producer.js
+++ producer.js
@@ -3,20 +3,19 @@
 const { Producer, KeyedMessage, Client } = require('kafka-node');
 
 const client = new Client('127.0.0.1:2181');
-const topic = 'first_topic';
-const partition = 0;
-const attributes = 0;
 const producer = new Producer(client, { requiredAcks: 1 });
 
-producer.on('ready', () => {
-  const keyedMessage = new KeyedMessage('keyed', 'another message');
-  const message = 'hello there';
+const createAndPublishMessage = () => {
+  const keyedMessage = new KeyedMessage('keyed', 'message');
+  const message = 'message';
   producer.send([
-    { topic, partition, messages: [message, keyedMessage], attributes }
+    { topic:'first_topic', partition: 0, messages: [message, keyedMessage], attributes: 0 }
   ], (err, result) => {
     console.log(err || result);
     process.exit();
   });
-});
+};
+
+producer.on('ready', createAndPublishMessage);
 
 producer.on('error', console.error);
