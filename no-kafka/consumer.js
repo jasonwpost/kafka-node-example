d1074b9e4bc363ef4c6868f31a07c27b1566af41
Jason Post <jason.w.post@gmail.com>
2017-12-13 10:43:06 +0100

    mulitple consumers

--- no-kafka/consumer.js
+++ no-kafka/consumer.js
@@ -1,10 +1,14 @@
 const { SimpleConsumer } = require('no-kafka');
-const consumer = new SimpleConsumer();
+const consumer1 = new SimpleConsumer();
+const consumer2 = new SimpleConsumer();
 
 const dataHandler = (messageSet, topic, partition) => {
   messageSet.forEach(message => console.log(topic, partition, message.offset, message.message.value.toString('utf8')));
 };
 
-return consumer.init().then(() =>
-  consumer.subscribe('first_topic', dataHandler)
+return Promise.all([consumer1.init(), consumer2.init()]).then(() => {
+    consumer1.subscribe('first_topic', dataHandler);
+    consumer2.subscribe('second_topic', dataHandler);
+
+}
 );
